3d3a06656abadbd2ad849b9fe73b19a062b92516
胡伟煌 <huweihuang@foxmail.com>
2023-11-22 18:22:04 +0800

    add swagger (#15)

--- pkg/types/apis.go
+++ pkg/types/apis.go
@@ -1,7 +1,25 @@
 package types
 
-type Response struct {
-	Code    int         `json:"code"`
-	Message string      `json:"message"`
-	Data    interface{} `json:"data"`
+type CommonResp struct {
+	Code    int    `json:"code"`
+	Message string `json:"message"`
+}
+
+type ErrorResp struct {
+	CommonResp
+	Data struct {
+		Error string `json:"error"`
+	} `json:"data"`
+}
+
+type CreateInstanceResp struct {
+	CommonResp
+	Data struct {
+		JobID string `json:"jobId"`
+	} `json:"data"`
+}
+
+type GetInstanceResp struct {
+	CommonResp
+	Data *Instance `json:"data"`
 }
